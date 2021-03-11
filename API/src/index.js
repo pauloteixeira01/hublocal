795dc314718250d5cac4014d850d32a270795309
Paulo Teixeira <paulo.teixeira541@gmail.com>
2021-03-10 21:15:00 -0300

    I updated 'index.js'

--- API/src/index.js
+++ API/src/index.js
@@ -8,5 +8,5 @@ const BusinessRoutes = require('./routes/BusinessRoutes');
 server.use('/task', BusinessRoutes);
 
 server.listen(3333, () => {
-	console.log('API online');
+	console.log('API online in port 3333');
 })
